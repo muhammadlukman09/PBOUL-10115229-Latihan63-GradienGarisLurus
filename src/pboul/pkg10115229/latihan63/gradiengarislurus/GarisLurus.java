5a82e4951ad5ebf3f3e3a743414b64f3e1308439
Petani Kode <muhammadlukman09@gmail.com>
2019-10-25 03:18:29 +0700

    project enamtiga

---
+++ src/pboul/pkg10115229/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Lukman
+ * NIM      : 10115229
+ * NAMA     : MUHAMMAD LUKMAN HAKIM
+ * KELAS    : PBO ULANG
+ */
+public interface GarisLurus {
+    void hitungGradien()
+    
+    
+    
+    
+}
---
+++ src/pboul/pkg10115229/latihan63/gradiengarislurus/PBOUL10115229Latihan63GradienGarisLurus.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author Lukman
+ * NIM      : 10115229
+ * NAMA     : MUHAMMAD LUKMAN HAKIM
+ * KELAS    : PBO ULANG
+ */
+public class PBOUL10115229Latihan63GradienGarisLurus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
